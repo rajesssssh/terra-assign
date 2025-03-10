2ab3b2b2cec02481301a3f8f2a713740fc741161
rajesssssh <rajesh0807r@gmail.com>
2025-03-10 06:35:44 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "webserver_access" {
 
 resource "aws_instance" "ourfirst" {
   ami                    = "ami-00bb6a80f01f03502"
-  availability_zone      = "ap-south-1"
+  availability_zone      = "ap-south-1a"
   instance_type          = "t2.micro"
   user_data              = filebase64("install_ansible.sh")
   vpc_security_group_ids = [aws_security_group.webserver_access.id]  # Correct reference
